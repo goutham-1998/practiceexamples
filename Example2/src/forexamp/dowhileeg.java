8fe19af35365b698cd843b4f88f6fc70a99ebe85
Goutham Gummadipudi <ggummadipudi@gmail.com>
2022-02-18 15:44:11 +0530

    Changes have been made

---
+++ Example2/src/forexamp/dowhileeg.java
@@ -0,0 +1,26 @@
+package forexamp;
+
+import java.util.Scanner;
+
+public class dowhileeg {
+
+	public static void main(String[] args) {
+		int i;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the value = ");
+		i=sc.nextInt();
+		do 
+		{
+			System.out.println(i);
+			i++;
+		}
+		while (i<=5);
+		{
+			
+			System.out.println(i);
+			
+		}
+			
+	}
+
+}
