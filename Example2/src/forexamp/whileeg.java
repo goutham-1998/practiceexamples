fb2aa58277d9b5909f52b61b64f18bc2181e32ff
Goutham Gummadipudi <ggummadipudi@gmail.com>
2022-02-18 15:34:58 +0530

    Changes done for example 2

---
+++ Example2/src/forexamp/whileeg.java
@@ -0,0 +1,26 @@
+package forexamp;
+
+import java.util.Scanner;
+
+public class whileeg {
+
+	public static void main(String[] args) {
+		int i;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number of values = ");
+		i=sc.nextInt();
+		while (i<=5)
+		{
+			System.out.println(i);
+			i++;
+		}
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
