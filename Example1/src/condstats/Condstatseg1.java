e25e945e89e75ce06ac22bea49825fcc6e93794b
Goutham Gummadipudi <ggummadipudi@gmail.com>
2022-02-18 14:58:23 +0530

    Changes have been comitted

---
+++ Example1/src/condstats/Condstatseg1.java
@@ -0,0 +1,24 @@
+package condstats;
+
+public class Condstatseg1 {
+
+	public static void main(String[] args) {
+		int num = 0;
+		
+		if (num > 0)
+		{
+			System.out.println(num+" is a +ve number");
+		}
+		else if (num < 0)
+		{
+			System.out.println(num+" is a -ve number");
+		}
+		else
+		{
+			System.out.println(num+" is a Null");
+		}
+		
+
+	}
+
+}
